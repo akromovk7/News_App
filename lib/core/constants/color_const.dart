609d84ecc13f3cf8647b16de4583fab65d1d6183
akromovkamoliddin <akromovkwork@gmail.com>
2022-07-18 10:48:13 +0500

    first commit

---
+++ lib/core/constants/color_const.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ConsColors {
+  static const Color kPrimaryColor = Color(0XFFFF3A44);
+  static const Color kSecondaryColor = Color(0XFF0080FF);
+  static const Color kTretiaryColor = Color(0XFFFFE600);
+}
---
+++ lib/core/constants/font_sizes.dart
@@ -0,0 +1,8 @@
+class FontConst {
+  static double kMinSmallFont = 14.0;
+  static double kSmallFont = 16.0;
+  static double kMaxSmallFont = 18.0;
+  static double kMinMediumFont = 20.0;
+  static double kMediumFont = 24.0;
+  static double kMaxMediumFont = 28.0;
+}
